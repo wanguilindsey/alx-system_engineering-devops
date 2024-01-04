4689eca8c440414fad500a5dc9caf82849beb0a0
wanguilindsey <wanguilindsey@gmail.com>
2024-01-04 11:05:15 -0800

    regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-
-# Get the first command-line argument
-string_to_match = ARGV[0]
-
-# Define the regular expression using Oniguruma syntax
-regex = /School/
-
-# Check if the string matches the regular expression
-if regex.match(string_to_match)
-  # If there is a match, print the matched part of the string
-  puts string_to_match.match(regex)[0] + "$"
-else
-  # If there is no match, print an empty line
-  puts "$"
-end
+puts ARGV[0].scan(/School/).join
