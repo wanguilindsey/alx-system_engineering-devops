efb8c5c3f647a2b1621a391f0e9fea91c3c09d76
wanguilindsey <wanguilindsey@gmail.com>
2024-06-12 22:50:24 -0700

    update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-# Change the OS configuration so that it is possible to login with the
+# Change the OS configuration so that it's possible to login with the
 # holberton user and open a file without any error message.
 
 exec {'OS security config':
